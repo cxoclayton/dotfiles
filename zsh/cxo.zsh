1709acea88b4688508824f22b0e4a43d2f4428aa
Clayton Coleman <clayton@cxodesigns.net>
2020-11-06 23:10:23 -0500

    Added ldd alias

--- zsh/cxo.zsh
+++ zsh/cxo.zsh
@@ -3,6 +3,7 @@ dev=~/dev/
 alias zrl='source ~/.zshrc'
 alias ldw='docker-compose exec --user=laradock workspace $1'
 alias ldup='docker-compose up -d nginx mariadb redis'
+alias ldd='docker-compose down'
 
 fpath() {
  d=`pwd`
